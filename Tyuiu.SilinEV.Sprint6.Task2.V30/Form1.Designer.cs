c5aa489732fc755d5b5287163af0a5d0d5c896e3
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-12-14 00:18:55 +0500

    Task2

--- Tyuiu.SilinEV.Sprint6.Task2.V30/Form1.Designer.cs
+++ Tyuiu.SilinEV.Sprint6.Task2.V30/Form1.Designer.cs
@@ -28,12 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            fontDialog1 = new FontDialog();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.формула2;
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(322, 50);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(12, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(204, 37);
+            label1.TabIndex = 1;
+            label1.Text = "Стартовый шаг";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 20F);
+            textBox1.Location = new Point(234, 68);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 43);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 20F);
+            button1.Location = new Point(12, 178);
+            button1.Name = "button1";
+            button1.Size = new Size(165, 59);
+            button1.TabIndex = 3;
+            button1.Text = "Вычислить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 20F);
+            label2.Location = new Point(12, 112);
+            label2.Name = "label2";
+            label2.Size = new Size(198, 37);
+            label2.TabIndex = 4;
+            label2.Text = "Конечный шаг";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 20F);
+            textBox2.Location = new Point(234, 117);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 43);
+            textBox2.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2 });
+            dataGridView1.Location = new Point(340, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(168, 319);
+            dataGridView1.TabIndex = 6;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "x";
+            Column1.Name = "Column1";
+            Column1.Width = 40;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "F(x)";
+            Column2.Name = "Column2";
+            Column2.Width = 60;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(527, 31);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(323, 300);
+            chart1.TabIndex = 7;
+            chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(864, 348);
+            Controls.Add(chart1);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label2;
+        private TextBox textBox2;
+        private DataGridView dataGridView1;
+        private FontDialog fontDialog1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
     }
 }
--- Tyuiu.SilinEV.Sprint6.Task2.V30/Form1.cs
+++ Tyuiu.SilinEV.Sprint6.Task2.V30/Form1.cs
@@ -1,10 +1,39 @@
+using Tyuiu.SilinEV.Sprint6.Task2.V30.Lib;
+
 namespace Tyuiu.SilinEV.Sprint6.Task2.V30
 {
     public partial class Form1 : Form
     {
+        DataService ds = new DataService();
         public Form1()
         {
             InitializeComponent();
+            this.chart1.Titles.Add("График ф-ции");
+            this.chart1.ChartAreas[0].AxisX.Title = "Ось Х";
+            this.chart1.ChartAreas[0].AxisY.Title = "Ось Y";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.dataGridView1.Rows.Clear();
+                this.chart1.Series[0].Points.Clear();
+
+                int a = Convert.ToInt32(textBox1.Text), s = Convert.ToInt32(textBox2.Text);
+                double[] d = ds.GetMassFunction(a, s);
+
+
+                for (int i = 0; i < d.Length; i++)
+                {
+                    this.dataGridView1.Rows.Add(Convert.ToString(a), Convert.ToString(d[i]));
+                    this.chart1.Series[0].Points.AddXY(a, d[i]);
+                    a++;
+                }
+            }
+            catch
+            {
+            }
         }
     }
 }
