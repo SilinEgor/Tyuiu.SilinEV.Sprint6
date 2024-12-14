e38d1837681c402c9fd866ce2ac0ff7bdd9e9397
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-12-14 10:26:40 +0500

    Task3

--- Tyuiu.SilinEV.Sprint6.Task3.V19/Form1.Designer.cs
+++ Tyuiu.SilinEV.Sprint6.Task3.V19/Form1.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.масив;
+            pictureBox1.Location = new Point(22, 22);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(168, 216);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 20F);
+            button1.Location = new Point(22, 254);
+            button1.Name = "button1";
+            button1.Size = new Size(181, 59);
+            button1.TabIndex = 4;
+            button1.Text = "Сортировка";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5 });
+            dataGridView1.Location = new Point(209, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(250, 301);
+            dataGridView1.TabIndex = 7;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "1";
+            Column1.Name = "Column1";
+            Column1.Width = 40;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "2";
+            Column2.Name = "Column2";
+            Column2.Width = 40;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "3";
+            Column3.Name = "Column3";
+            Column3.Width = 40;
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "4";
+            Column4.Name = "Column4";
+            Column4.Width = 40;
+            // 
+            // Column5
+            // 
+            Column5.HeaderText = "5";
+            Column5.Name = "Column5";
+            Column5.Width = 40;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(569, 345);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
     }
 }
--- Tyuiu.SilinEV.Sprint6.Task3.V19/Form1.cs
+++ Tyuiu.SilinEV.Sprint6.Task3.V19/Form1.cs
@@ -1,3 +1,5 @@
+using Tyuiu.SilinEV.Sprint6.Task3.V19.Lib;
+
 namespace Tyuiu.SilinEV.Sprint6.Task3.V19
 {
     public partial class Form1 : Form
@@ -5,6 +7,30 @@ namespace Tyuiu.SilinEV.Sprint6.Task3.V19
         public Form1()
         {
             InitializeComponent();
+        }
+        DataService ds = new DataService();
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[,] a = ds.Calculate(ds.qwe("4  32  -20  27  21  17  15  -1  -2  -1  -3  18  12  -10  29  7  -15  2  -8  12  -10  25  5  27  21"));
+            this.dataGridView1.Rows.Clear();
+
+            try
+            {
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    this.dataGridView1.Rows.Add(Convert.ToString(a[i, 0]), Convert.ToString(a[i, 1]), Convert.ToString(a[i, 2]), Convert.ToString(a[i, 3]), Convert.ToString(a[i, 4]));
+                }
+            }
+            catch
+            {
+
+            }
         }
     }
 }
