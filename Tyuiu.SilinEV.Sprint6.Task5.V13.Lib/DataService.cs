dac20ddf107c27b7c6a3da039e7b0966ac9055d2
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-12-19 13:28:21 +0500

    Task5

---
+++ Tyuiu.SilinEV.Sprint6.Task5.V13.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.SilinEV.Sprint6.Task5.V13.Lib
+{
+    public class DataService : ISprint6Task5V13
+    {
+        public double[] LoadFromDataFile(string path)
+        {
+            string[] a = File.ReadAllText(path).Replace(".", ",").Split(" ");
+
+            double[] s = new double[1];
+            int d = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (Convert.ToDouble(a[i]) < 10)
+                {
+                    s[d] = Convert.ToDouble(a[i]);
+                    d++;
+                    Array.Resize(ref s, d + 1);
+                }
+            }
+
+            Array.Resize(ref s, d);
+
+            return s;
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint6.Task5.V13.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint6.Task5.V13.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
