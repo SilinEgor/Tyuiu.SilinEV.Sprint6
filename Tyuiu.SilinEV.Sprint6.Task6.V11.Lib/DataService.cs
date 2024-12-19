6645e58d4332cb35bc462ac83c2331d12ab8e6be
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-12-19 15:52:16 +0500

    Task6

---
+++ Tyuiu.SilinEV.Sprint6.Task6.V11.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.SilinEV.Sprint6.Task6.V11.Lib
+{
+    public class DataService : ISprint6Task6V11
+    {
+        public string CollectTextFromFile(string str, string path)
+        {
+            string a = "";
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    a += line[line.Length - 1] + " ";
+                }
+            }
+
+            return a.Trim();
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint6.Task6.V11.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint6.Task6.V11.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
